a1cc404b405ed22b707a248a0aeb949d7f878ada
Rita Jackson <ritaandahedgehog@gmail.com>
2013-11-26 15:49:02 -0500

    Added the ‘back’ functionality to the Directions Input screen.

--- src/com/smartcity/redux/DirectionsInput.java
+++ src/com/smartcity/redux/DirectionsInput.java
@@ -1,9 +1,13 @@
 package com.smartcity.redux;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -13,6 +17,9 @@ public class DirectionsInput extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_directions_input);
+		
+		setupActionBar();
+
 	}
 
 	@Override
@@ -35,6 +42,29 @@ public class DirectionsInput extends Activity {
 		intent.putExtra("destination", destinationText);
 		
 		DirectionsInput.this.startActivity(intent);
+	}
+	
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
--- src/com/smartcity/redux/MainActivity.java
+++ src/com/smartcity/redux/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends Activity {
 		MainActivity.this.startActivity(intent);
 	}
 	
+	
 	public void startHoboken311Activity(View view) {
 		Intent intent = new Intent(MainActivity.this,Hoboken311Activity.class);
 		MainActivity.this.startActivity(intent);
