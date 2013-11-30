0a266ce1034126eb8ed3e1cc746d405a197258d0
dschuler2 <dschuler@stevens.edu>
2013-11-30 16:49:59 -0500

    Began adding table for showing all previous gas consumption entries.

--- src/com/smartcity/redux/fragments/ViewGasConsumptionFragment.java
+++ src/com/smartcity/redux/fragments/ViewGasConsumptionFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.smartcity.redux.R;
 
@@ -24,6 +27,30 @@ public class ViewGasConsumptionFragment extends Fragment {
 				.findViewById(R.id.section_label);
 		dummyTextView.setText(Integer.toString(getArguments().getInt(
 				ARG_SECTION_NUMBER)));*/
+		TableLayout tableLayout = (TableLayout) rootView.findViewById(R.id.gasResultsTable);
+		TableRow tableRow = new TableRow(getActivity());
+		tableRow.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
+		
+		TextView text1 = new TextView(getActivity());
+		text1.setText("11/30/2013");
+		text1.setPadding(15, 0, 15, 0);
+		text1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+		
+		TextView text2 = new TextView(getActivity());
+		text2.setText("45");
+		text2.setPadding(15, 0, 15, 0);
+		text2.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+		
+		TextView text3 = new TextView(getActivity());
+		text3.setText("$98.45");
+		text3.setPadding(15, 0, 15, 0);
+		text3.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+		
+		tableRow.addView(text1);
+		tableRow.addView(text2);
+		tableRow.addView(text3);
+		
+		tableLayout.addView(tableRow);
 		return rootView;
 	}
 }
