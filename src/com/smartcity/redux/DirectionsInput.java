dc71a66a75ef9cad647166136d056400b1a82c2c
tenoritaiga <ringbearer91@gmail.com>
2013-12-03 04:26:47 -0500

    Adding location suggestions and Google Places API integration

--- src/com/smartcity/redux/DirectionsInput.java
+++ src/com/smartcity/redux/DirectionsInput.java
@@ -1,26 +1,122 @@
 package com.smartcity.redux;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.smartcity.redux.adapters.PlacesAutoCompleteAdapter;
+
 import android.os.Build;
 import android.os.Bundle;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class DirectionsInput extends Activity {
+public class DirectionsInput extends Activity implements OnItemClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_directions_input);
 		
+		AutoCompleteTextView autoCompView = (AutoCompleteTextView) findViewById(R.id.destinationInput);
+		autoCompView.setAdapter(new PlacesAutoCompleteAdapter(this, android.R.layout.test_list_item));
+		autoCompView.setOnItemClickListener(this);
+		
+		 API_KEY = getResources().getText(R.string.MapsAPIKey).toString();
+		 
+		 Log.d("APIKEY",API_KEY);
+		
 		setupActionBar();
 
 	}
+	
+	public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        String str = (String) adapterView.getItemAtPosition(position);
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+    }
+
+	
+	private static final String LOG_TAG = "SmartCity";
+	private String API_KEY;
+	private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+	private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+	private static final String OUT_JSON = "/json";
+
+	public ArrayList<String> autocomplete(String input) {
+		
+	    ArrayList<String> resultList = null;
+
+	    HttpURLConnection conn = null;
+	    StringBuilder jsonResults = new StringBuilder();
+	    try {
+	        StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+	        sb.append("?sensor=false&key=" + API_KEY);
+	        sb.append("&components=country:us");
+	        sb.append("&input=" + URLEncoder.encode(input, "utf8"));
+
+	        URL url = new URL(sb.toString());
+	        conn = (HttpURLConnection) url.openConnection();
+	        InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+	        // Load the results into a StringBuilder
+	        int read;
+	        char[] buff = new char[1024];
+	        while ((read = in.read(buff)) != -1) {
+	            jsonResults.append(buff, 0, read);
+	        }
+	    } catch (MalformedURLException e) {
+	        Log.e(LOG_TAG, "Error processing Places API URL", e);
+	        return resultList;
+	    } catch (IOException e) {
+	        Log.e(LOG_TAG, "Error connecting to Places API", e);
+	        return resultList;
+	    } finally {
+	        if (conn != null) {
+	        	//Log.d("DISCONNECT","OK, disconnecting from Places API");
+	            conn.disconnect();
+	        }
+	    }
+
+	    try {
+	        // Create a JSON object hierarchy from the results
+	        JSONObject jsonObj = new JSONObject(jsonResults.toString());
+	        Log.d("JSON", jsonResults.toString());
+	        JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
+
+	        // Extract the Place descriptions from the results
+	        resultList = new ArrayList<String>(predsJsonArray.length());
+	        for (int i = 0; i < predsJsonArray.length(); i++) {
+	            resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+	        }
+	    } catch (JSONException e) {
+	        Log.e(LOG_TAG, "Cannot process JSON results", e);
+	    }
+	    
+	    Log.d("RESULTS SIZE",Integer.toString(resultList.size()));
+	    //Log.d("RESULT 1",resultList.get(0));
+
+	    return resultList;
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
