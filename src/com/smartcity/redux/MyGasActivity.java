87608b6485b4867c8971b273d3a7b2fc5233b7d9
dschuler2 <dschuler@stevens.edu>
2013-12-18 21:20:34 -0500

    Began implementation of API function calls for My Gas Consumption functionality.

--- src/com/smartcity/redux/MyGasActivity.java
+++ src/com/smartcity/redux/MyGasActivity.java
@@ -2,20 +2,40 @@ package com.smartcity.redux;
 
 
 
-import com.smartcity.redux.adapters.GasPagerAdapter;
-import com.smartcity.redux.fragments.DatePickerFragment;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.ActionBar;
-import android.app.DialogFragment;
 import android.app.FragmentTransaction;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.smartcity.redux.adapters.GasPagerAdapter;
+import com.smartcity.redux.fragments.DatePickerFragment;
 
 public class MyGasActivity extends FragmentActivity implements
 		ActionBar.TabListener {
@@ -76,6 +96,8 @@ public class MyGasActivity extends FragmentActivity implements
 					.setText(mSectionsPagerAdapter.getPageTitle(i))
 					.setTabListener(this));
 		}
+		
+		new JsonGasAvgParser().execute();
 	}
 
 	@Override
@@ -123,6 +145,118 @@ public class MyGasActivity extends FragmentActivity implements
 	public void showDatePickerDialog(View v) {
 		DatePickerFragment newFragment = new DatePickerFragment();
 		newFragment.show(getSupportFragmentManager(), "datePicker");
+	}
+	
+	private static InputStream retrieveStream(String url) {
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpGet getRequest = new HttpGet(url);
+		
+		try{
+			HttpResponse getResponse = client.execute(getRequest);
+			final int statusCode = getResponse.getStatusLine().getStatusCode();
+			System.out.println(statusCode);
+			
+			if(statusCode != HttpStatus.SC_OK){
+				//Log.w(getClass().getSimpleName(),
+						//"Error " + statusCode + " for URL " + url);
+				System.out.println(statusCode);
+				return null;
+			}
+			
+			HttpEntity getResponseEntity = getResponse.getEntity();
+			return getResponseEntity.getContent();
+		}
+		
+		catch(IOException e){
+			getRequest.abort();
+			//Log.w(getClass().getSimpleName(),"Error for URL " + url, e);
+		}
+		return null;
+	}
+	
+	private class JsonGasAvgParser extends AsyncTask<Void,Void,JSONObject> {
+		
+		@Override
+		protected JSONObject doInBackground(Void... params) {
+			String url = "http://50.17.51.160/api/UserGasConsumptionAverages?user_id=9";
+			InputStream stream = retrieveStream(url);
+			Log.d("STREAM", (stream == null) + "");
+			
+			try{
+				Reader reader = new InputStreamReader(stream);
+				BufferedReader bufRead = new BufferedReader(reader);
+				StringBuilder builder = new StringBuilder();
+				String line;
+				while ((line = bufRead.readLine()) != null) {
+					builder.append(line);
+					System.out.println(line);
+				}
+				String jsonString = builder.toString();
+				JSONObject jsonObject = new JSONObject(jsonString);
+				//System.out.println(new JSONObject(jsonObject.getString("HomeAddress")).getString("NumberAndStreet"));
+				return jsonObject;
+			}
+			catch(NullPointerException npe){
+				Log.e("ERROR","Warning: Null pointer exception while parsing JSON!",npe);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(JSONObject jsonObject) {
+			
+			try {
+				EditText editText = (EditText) findViewById(R.id.cons_avg);
+				editText.setText(jsonObject.getString("UserAverage30Day"));
+				editText = (EditText) findViewById(R.id.cons_avg_all);
+				editText.setText(jsonObject.getString("CityAverage30Day"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private class JsonGasSender extends AsyncTask<Void,Void,Void> {
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			JSONObject json = new JSONObject();
+			
+			try {
+				json.put("UserID", 9);
+				TextView textView = (TextView) findViewById(R.id.view_date);
+				json.put("Date", textView.getText().toString());
+				EditText editText = (EditText) findViewById(R.id.edit_gallons);
+				json.put("Gallons", Double.parseDouble(editText.getText().toString()));
+				editText = (EditText) findViewById(R.id.edit_cost);
+				json.put("Cost", Double.parseDouble(editText.getText().toString()));
+				
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpPost postRequest  = new HttpPost("http://50.17.51.160/api/GasConsumption");
+				System.out.println(json.toString());
+				StringEntity se = new StringEntity(json.toString());
+				se.setContentType("application/json");
+				se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+				postRequest.setEntity(se);
+				HttpResponse response = client.execute(postRequest);
+				System.out.println(response.getStatusLine().getStatusCode());
+				return null;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+	
+	public void saveGasConsumption(View view) {
+		new JsonGasSender().execute();
 	}
 
 }
