ff0f5f7acc5e64851a7ea6e0fc8f2a7035dea6e0
Rita Jackson <ritaandahedgehog@gmail.com>
2014-01-30 23:28:46 -0500

    Added PSE&G Energy Calculator. NOT MOBILE FRIENDLY! Need to look into a mobile friendly solution.

--- src/com/smartcity/redux/EnergyCalculatorActivity.java
+++ src/com/smartcity/redux/EnergyCalculatorActivity.java
@@ -3,6 +3,7 @@ package com.smartcity.redux;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
 
 public class EnergyCalculatorActivity extends Activity {
 
@@ -10,13 +11,20 @@ public class EnergyCalculatorActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_energy_calculator);
+		
+		 WebView webview = new WebView(this);
+		 setContentView(webview);
+		 
+		 webview.loadUrl("http://c03.apogee.net/calcs/rescalc5x/Profile.aspx");
 	}
 
+	/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.energy_calculator, menu);
 		return true;
 	}
+	*/
 
 }
