682c377447490bef4dbdcdfef3da2811b455626b
dschuler2 <dschuler@stevens.edu>
2014-04-16 10:31:55 -0400

    Made a small fix so that the new Sustainability activity can also be launched from the sliding menu.

--- src/com/smartcity/redux/fragments/SustainabilityCategoryFragment.java
+++ src/com/smartcity/redux/fragments/SustainabilityCategoryFragment.java
@@ -1,36 +1,22 @@
 package com.smartcity.redux.fragments;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Fragment;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 
-import com.jjoe64.graphview.BarGraphView;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.smartcity.redux.R;
+import com.smartcity.redux.SustainabilityCategoryActivity;
 
 public class SustainabilityCategoryFragment extends Fragment {
 	
-	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+		Intent intent = new Intent(getActivity(), SustainabilityCategoryActivity.class);
+		getActivity().startActivity(intent);
+		return inflater.inflate(R.layout.fragment_sustainability_category, null);
+	}
 }
